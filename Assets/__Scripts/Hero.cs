5e61a52b84277da1cb7ad6819f13ee5f9254e736
Bill Crosbie <crosbie.git@gmail.com>
2017-10-06 20:02:08 -0400

    Initial Commit

---
+++ Assets/__Scripts/Hero.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	static public Hero		S;
+
+	public float	speed = 30;
+	public float	rollMult = -45;
+	public float  	pitchMult=30;
+
+	public float	shieldLevel=1;
+
+	public bool	_____________________;
+	public Bounds bounds;
+
+	void Awake(){
+		S = this;
+		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xAxis = Input.GetAxis("Horizontal");
+		float yAxis = Input.GetAxis("Vertical");
+
+		Vector3 pos = transform.position;
+		pos.x += xAxis * speed * Time.deltaTime;
+		pos.y += yAxis * speed * Time.deltaTime;
+		transform.position = pos;
+		
+		bounds.center = transform.position;
+		
+		// constrain to screen
+		Vector3 off = Utils.ScreenBoundsCheck(bounds,BoundsTest.onScreen);
+		if (off != Vector3.zero) {  // we need to move ship back on screen
+			pos -= off;
+			transform.position = pos;
+		}
+		
+		// rotate the ship to make it feel more dynamic
+		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
+	}
+}
